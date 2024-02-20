2451c34a265dac6712ba82a6be301aca29ddff35
tatsuya <86834007+ta282ji59@users.noreply.github.com>
2024-02-20 11:32:49 +0900

    Update holidaysUpdate.js

--- holidaysUpdate.js
+++ holidaysUpdate.js
@@ -1,6 +1,13 @@
 function holiday() {
   const sheet_holidays = sheet('holidays');
-  sheet_holidays.clear();  
-  const range = sheet_holidays.getRange('A1');
-  range.setValue('=IMPORTDATA("https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv")');
+  sheet_holidays.clear();
+  var response = UrlFetchApp.fetch("https://holidays-jp.github.io/api/v1/date.json");
+  var holidays = JSON.parse(response.getContentText());
+  
+  var row = 2; // Start from the second row
+  for (var date in holidays) {
+    sheet_holidays.getRange(row, 1).setValue(new Date(date).toISOString().slice(0,10));
+    sheet_holidays.getRange(row, 2).setValue(holidays[date]);
+    row++;
+  }
 }
